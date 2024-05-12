06f9cf2c4fd86ead17125b921b6c2d37e7f46a5e
나유경 <zz6cod@gmail.com>
2024-05-13 00:57:27 +0900

    Redis로 간단한 API 만들기 (LPUSH, LRANGE)

---
+++ app/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { RedisClientType } from "redis";
+
+export const LIST_KEY = "messages";
+
+export type RedisClient = RedisClientType<any, any, any>;
+
+export const createApp = (client: RedisClient) => {
+    const app = express();
+
+    app.use(express.json()) // json 형식 받기 위해 json 파싱 하는 것 넣음.
+
+    app.get("/", (request, response) => {
+        response.status(200).send("hello from express");
+    });
+
+    app.post("/messages", async (request, response) => {
+        const { message } = request.body;
+        await client.lPush(LIST_KEY, message);
+        response.status(200).send("Message added to list"); // 저장 성공 알림
+    });
+
+    app.get("/messages", async (request, response) => {
+        const messages = await client.lRange(LIST_KEY, 0, -1);
+        response.status(200).send(messages);
+    })
+
+    return app;
+};
